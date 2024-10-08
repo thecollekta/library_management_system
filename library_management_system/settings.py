ba63c1987821db05c090fc73ea42c3ba65f7eebb
thecollekta <festus233@gmail.com>
2024-10-08 13:03:19 +0000

    Updated the ALLOWED_HOSTS

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = ['lmsapi.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['libmgtsys.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
